5c901603d45e62d45d17cfbfc6a9b3ddee33eee2
Dan Chung <danetix@gmail.com>
2018-08-08 19:52:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(collection, keyword) {
+  return collection.filter(item => item.toLowerCase() === keyword.toLowerCase())
+}
+
+function fuzzyMatch(collection, keyword) {
+  return collection.filter(item => item.slice(0, keyword.length) === keyword)
+}
+
+function matchName(collection, keyword) {
+  return collection.filter(item => item.name === keyword)
+}
