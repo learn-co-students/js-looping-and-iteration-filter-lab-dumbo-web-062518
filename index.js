3201176eacf3ca23c5f21b7d5370cb90b86ae2a0
alexneustein <alex.neustein@gmail.com>
2018-08-15 17:44:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+  const matches = collection.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
+  return matches;
+}
+
+function fuzzyMatch(collection, query) {
+  const searchLength = query.length;
+  const matches = collection.filter(function (driver) {
+    return ((driver.toLowerCase().slice(0,query.length)) === query.toLowerCase()) });
+  return matches;
+  }
+
+function matchName(collection, query) {
+  const matches = collection.filter(
+    function (driver) {
+      return (driver.name === query)
+    }
+  )
+  return matches
+}
